3d8c7c57301056b66a4238ea51a91a1e639082f2
mspur054 <mspur054@uottawa.ca>
2020-12-15 17:29:59 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h5>This is hello </h5>
+        <h5>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h5>
         <a
           className="App-link"
           href="https://reactjs.org"
